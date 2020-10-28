69028dc250f4c8073787230f860b4acc0e4a7370
Peter Trinh <peter@particlespace.com>
2020-10-28 02:37:49 -0500

    Initialize chatbot

---
+++ App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
+
+class App extends Component {
+  state = {
+    messages: [
+      {
+        _id: 1,
+        text: `Hi! I am the FAQ bot 🤖 from Jscrambler.\n\nHow may I help you with today?`,
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'FAQ Bot',
+          avatar: 'https://i.imgur.com/7k12EPD.png'
+        }
+      }
+    ]
+  };
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages)
+    }));
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1, backgroundColor: '#fff' }}>
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{
+            _id: 1
+          }}
+        />
+      </View>
+    );
+  }
+}
+
+export default App;
